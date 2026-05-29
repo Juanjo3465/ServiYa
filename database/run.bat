ad2267d63a783437c0097f80f43c21890a8f9ee5
Juan Beltran <jbeltrano@unal.edu.co>
2026-05-29 16:24:12 -0500

    Mod to script run.bat

--- database/run.bat
+++ database/run.bat
@@ -6,15 +6,15 @@ docker compose up -d
 
 
 echo "Waiting for MySQL to be ready..."
-timeout /t 60 > nul
+timeout /t 30 > nul
 
 
 rem Execute SQL init script (if exist)
 if exist "create_database.sql" (
 
     echo " Excecute SQl init script..."
-    docker exec -i serviya-mysql mysql -u root -p${SPRING_DB_PASSWORD_ROOT} ${SPRING_DB_NAME} < ./create_database.sql
-
+    rem docker exec -i serviya-mysql mysql -u root -p${SPRING_DB_PASSWORD_ROOT} ${SPRING_DB_NAME} < create_database.sql
+    docker exec -i serviya-mysql mysql -u root -proot marketplace_services < create_database.sql
     echo " Script SQL excecuted successfully."
 ) else (
     echo " create_database.sql not found. Skipping SQL initialization."
