391461bbe398138627fc9b21cffb3216c47dc3f2
Juan Beltran <jbeltrano@unal.edu.co>
2026-06-13 12:16:28 -0500

    Fixed error whit services entity, and add .sql to create test categories and test services

---
+++ mysql/05_populate_categories_database.sql
@@ -0,0 +1,10 @@
+insert into categories (name) values 
+('Electricidad'),
+('Fontanería'),
+('Carpintería'),
+('Pintura'),
+('Jardinería'),
+('Limpieza'),
+('Reparaciones de electrodomésticos'),
+('Reparaciones de vehículos'),
+('Tecnología y electrónica');
